3c14c0ca5a34e4dd20d98eef15392945fec24c94
Martin Winther <mswinther@icloud.com>
2023-07-03 08:15:18 +0200

    Update main.jsx
    Refactor

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import Navbar from "./components/Navbar.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Navbar />
-		<App />
+		<div className="container">
+			<Navbar />
+			<App />
+		</div>
 	</React.StrictMode>
 );
